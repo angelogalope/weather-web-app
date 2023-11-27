f1c742c44337de4598cbf5cb962d8f145618b94f
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2023-11-27 22:21:02 +0800

    Updated

--- resources/src/App.jsx
+++ resources/src/App.jsx
@@ -1,45 +1,63 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { IoSearch } from "react-icons/io5";
 
 function App() {
   const [place, setPlace] = useState("");
-  const [weather, getWeather] = useState([]);
+  const [weather, setWeather] = useState(null);
   
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(`http://localhost/api/weather/${place}`);
+      setWeather(response.data.weather);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2 items-center justify-center w-full h-screen bg-[url('/images/bg-image.jpg')] bg-cover">
       <div className='flex gap-1 bg-black text-white bg-opacity-30 w-[352px] h-auto rounded-lg p-3 z-10'>
-        <label>Search place: </label>
         <input 
+          placeholder='Search place'
           type="text" 
           value={place}
           onChange={(e) => setPlace(e.target.value)}
-          className='w-[178px] text-sm outline-none border-b border-white bg-black bg-opacity-0'/>
-        <button className='font-bold' onClick={getWeather}>Search</button>
+          className='w-full text-sm outline-none border-b border-white bg-black bg-opacity-0'/>
+        <button className='font-bold' onClick={getWeather}><IoSearch /></button>
       </div>
 
-      {/* <img src="images/sunny-cloud.png" className='w-[232px] h-[232px] right-72 absolute z-0'/> */}
       <div className='flex flex-col bg-black text-white gap-3 bg-opacity-30 w-[352px] h-auto rounded-md p-5  z-10'>
         <div>
-          <h1 className='text-2xl'>Today at <span className='text-4xl font-semibold'>Sibagat</span></h1>
-          <h1>Country: Philippines</h1>
+          {weather && (
+            <>
+              <h1 className='text-2xl'>Today at <span className='text-4xl font-semibold'>{weather.name}</span></h1>
+              <h1>Country: {weather.sys.country}</h1>
+            </>
+          )}
         </div>
         <div className='flex gap-2'>
-          <div className='flex flex-col w-[162px] h-[162px] text-white bg-black bg-opacity-40 rounded-lg p-2'>
-            <h1 className='text-5xl font-semibold'>32.20°</h1>
-            <p>Sunny</p>
-            <div className='flex flex-col items-end'>
-              <img src="images/sunny.png" className='w-[72px] h-[72px]'/>
-            </div>
-          </div>
-          <div className='flex flex-col items-start w-[162px] h-[162px] text-white bg-black bg-opacity-40 rounded-lg p-2'>
-            <h1 className='text-xl font-semibold'>Coordinates</h1>
-            <h1 className='flex flex-col items-end'>Longitude: <span>125.6975</span></h1>
-            <h1 className='flex flex-col items-end'>Latitude: <span>8.8225</span></h1>
-          </div>
+          {weather && (
+            <>
+              <div className='flex flex-col w-[162px] h-[162px] text-white bg-black bg-opacity-40 rounded-lg p-2'>
+                <h1 className='text-5xl font-semibold'>{weather.main.temp}°</h1>
+                <p>{weather.weather[0].main}</p>
+                <div className='flex flex-col items-end'>
+                  <img src={`images/${weather.weather[0].icon}.png`} alt="weather-icon" className='w-[72px] h-[72px]'/>
+                </div>
+              </div>
+              <div className='flex flex-col items-start w-[162px] h-[162px] text-white bg-black bg-opacity-40 rounded-lg p-2'>
+                <h1 className='text-xl font-semibold'>Coordinates</h1>
+                <h1 className='flex flex-col items-end'>Longitude: <span>{weather.coord.lon}</span></h1>
+                <h1 className='flex flex-col items-end'>Latitude: <span>{weather.coord.lat}</span></h1>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
