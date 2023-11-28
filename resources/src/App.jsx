f691421542ddc80c6a9280727117f5b2d17ea565
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2023-11-28 14:08:23 +0800

    Added Features
    Dynamic background and music

--- resources/src/App.jsx
+++ resources/src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import { IoSearch } from "react-icons/io5";
 import Clock from './components/Clock';
+
+import defaultBgImage from '/images/bg-image.jpg';
+import bgClearSky from '/images/bgClearSky.png';
+import bgClearSkyNight from '/images/bgClearSkyNight.png';
+import bgRain from '/images/bgRain.png';
+import bgShowerRain from '/images/bgShowerRain.png';
+import bgThunderstorm from '/images/bgThunderstorm.png';
+import bgSnow from '/images/bgSnow.png';
+import bgFog from '/images/bgFog.png';
+
+import clearSkyMusic from '/bg-music/clearSkyMusic.MP3?url';
+import clearSkyMusicNight from '/bg-music/clearSkyMusicNight.MP3?url';
+import rainMusic from '/bg-music/rainMusic.MP3?url';
+import thunderstormMusic from '/bg-music/thunderstormMusic.MP3?url';
+import snowMusic from '/bg-music/snowMusic.MP3?url';
 
 import clearSky from '/images/clear-sky.png';
 import clearSkyNight from '/images/clear-skyNight.png';
@@ -20,6 +35,7 @@ import fog from '/images/fog.png';
 function App() {
   const [place, setPlace] = useState("");
   const [weather, setWeather] = useState(null);
+  const [bgSound, setBgSound] = useState(null);
   
   const getWeather = async () => {
     try {
@@ -29,6 +45,83 @@ function App() {
       console.error('Error fetching weather data:', error);
     }
   };
+
+  const defaultBackgroundImage = defaultBgImage;
+
+  const backgroundImageMapping = {
+    "01d": bgClearSky,
+    "02d": defaultBgImage,
+    "03d": defaultBgImage,
+    "04d": defaultBgImage,
+    "09d": bgShowerRain,
+    "10d": bgRain,
+    "11d": bgThunderstorm,
+    "13d": bgSnow,
+    "50d": bgFog,
+    
+    "01n": bgClearSkyNight,
+    "02n": bgClearSkyNight,
+    "03n": bgClearSkyNight,
+    "04n": bgClearSkyNight,
+    "09n": bgShowerRain,
+    "10n": bgRain,
+    "11n": bgThunderstorm,
+    "13n": bgSnow,
+    "50n": bgFog,
+  }
+
+  const backgroundImageStyle = {
+    backgroundImage: `url('${backgroundImageMapping[weather?.weather[0]?.icon] || defaultBackgroundImage}')`,
+  };
+
+  const soundMapping = {
+    "01d": clearSkyMusic,
+    "02d": clearSkyMusic,
+    "03d": clearSkyMusic,
+    "04d": clearSkyMusic,
+    "09d": rainMusic,
+    "10d": rainMusic,
+    "11d": thunderstormMusic,
+    "13d": snowMusic,
+    "50d": clearSkyMusic,
+    
+    "01n": clearSkyMusicNight,
+    "02n": clearSkyMusicNight,
+    "03n": clearSkyMusicNight,
+    "04n": clearSkyMusicNight,
+    "09n": rainMusic,
+    "10n": rainMusic,
+    "11n": thunderstormMusic,
+    "13n": snowMusic,
+    "50n": clearSkyMusicNight,
+  }
+
+  useEffect(() => {
+    if(weather) {
+        const soundFile = soundMapping[weather?.weather[0]?.icon] || clearSkyMusic; // Provide a default music file
+        const audio = new Audio(soundFile);
+        setBgSound(audio);
+    }
+  }, [weather]);
+
+  useEffect(() => {
+    if (bgSound) {
+      bgSound.play().catch((error) => {
+        console.error('Error playing audio:', error);
+      });
+    }
+  
+    return () => {
+      if (bgSound) {
+        bgSound.pause();
+        bgSound.currentTime = 0;
+      }
+    };
+  }, [bgSound]);
+
+  // const backgroundImageStyle = {
+  //   backgroundImage: `url('defaultBackgroundImage')`,
+  // };
 
   const weatherIconMapping = {
     "01d": clearSky,
@@ -53,11 +146,11 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center w-full h-screen bg-[url('/images/bg-image.jpg')] bg-cover">
+    <div style={backgroundImageStyle} className="flex flex-col gap-2 items-center justify-center w-full h-screen bg-cover">
       <div className='flex items-center gap-1 bg-black text-white text-xl font-semibold bg-opacity-30 w-auto h-auto rounded-lg p-3 top-5 right-5 absolute z-10'>
         <Clock />
       </div>
-      <div className='flex gap-1 bg-black text-white bg-opacity-30 w-[352px] h-auto rounded-lg p-3 z-10'>
+      <div className='flex gap-1 bg-black text-white bg-opacity-30 w-[352px] h-auto rounded-lg absolute top-5 p-3 z-10'>
         <input 
           placeholder='Search place'
           type="text" 
